eab0954a197d7cdaa36acc4084506b2a5b24bf7e
lannguyen8896 <nguyenthilan.ct@gmail.com>
2018-09-17 22:12:29 +0700

    ui header footer sidebar

--- admin_site/src/RouteContainer.tsx
+++ admin_site/src/RouteContainer.tsx
@@ -1,13 +1,23 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import {Home} from "./Screens/Home";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import SideBar from "./Components/SideBar/SideBar";
 
-const RouteContainer = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={Home} />
-    </div>
-  </Router>
-);
+class RouteContainer extends React.Component<any,any>{
+  render(){
+    return(
+      <Router>
+        <div>
+          <Header/>
+          <SideBar/>
+          <Route exact path="/" component={Home} />
+          <Footer/>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default RouteContainer;
--- admin_site/src/Screens/Home.tsx
+++ admin_site/src/Screens/Home.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export class Home extends React.Component<any, any> {
   render() {
-    return <h1>Hello Lan</h1>;
+    return <h1 style={{margin: "150px 0px 0px 300px"}}>Hello Lan</h1>;
   }
 }
